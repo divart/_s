2988b3bf6c5d1a0af0bc0796f85348d606f18c26
divart <divart@divart.pl>
2016-11-10 17:16:17 +0100

    Update header.php
    Displays the optional custom logo. Does nothing if the custom logo is not available.

--- header.php
+++ header.php
@@ -26,6 +26,13 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<?php
+			/*
+ 			* Displays the optional custom logo.
+ 			* Does nothing if the custom logo is not available.
+ 			*/
+			if ( function_exists( 'the_custom_logo' ) ) { the_custom_logo(); }
+			?>
+			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
